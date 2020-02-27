d7a4843069ccaa7dff769811aca57bd4441f7f77
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-27 10:59:58 -0800

    add some comments explaining the signin/signup

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import request from 'superagent';
 
 export default class Login extends Component {
+    // setting initial state for user signin/out for username and password
     state = {
         usernameSignIn: '',
         usernameSignUp: '',
@@ -10,11 +11,12 @@ export default class Login extends Component {
     }
 
     handleSignIn = async () => {
+        // making a request to our signin route on our API and checking with the data on our server
         const signIn = await request.post(`https://mysterious-springs-09274.herokuapp.com/api/auth/signin`, {
             email: this.state.usernameSignIn,
             password: this.state.passwordSignIn,
         })
-
+        // setting the user object into local storage to so we can navigate to other pages after we log in 
         localStorage.setItem('user', JSON.stringify(signIn.body));
         // this redirects the user after sign in
         this.props.history.push('/');
@@ -34,7 +36,8 @@ export default class Login extends Component {
     
     render() {
         return (
-            <div>Email*
+            // all of these inputs are going to set state to our inputted values and then run our handleSignUp and handleSignIn methods
+            <div>Email* 
                 <input value={ this.state.usernameSignUp} onChange={(e) => this.setState({ usernameSignUp: e.target.value})} />
                 Password*
                 <input value={ this.state.passwordSignUp} onChange={(e) => this.setState({ passwordSignUp: e.target.value})} />
